839f0802d9233371407c40925896345902cc0c54
jonyxz <johanridho08@gmail.com>
2024-10-22 16:02:57 +0700

    add restaurant profile data

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final restoData = <String, String>{};
+
+  RestoApp({super.key}) {
+    restoData['name'] = 'Rm. Sedap Rasa';
+    restoData['email'] = 'sedaprasa@gmail.com';
+    restoData['phone'] = '+6281234567890';
+    restoData['address'] = 'Jl. Diponegoro No.12, Semarang, Indonesia';
+    restoData['image'] = 'gambarWarung.jpg';
+    restoData['desc'] =
+        'Rumah makan yang menyajikan makanan lezat dengan suasana nyaman. Cocok untuk makan bersama keluarga.';
+    restoData['menu'] = '1. Ayam Goreng\n2. Nasi Goreng\n3. Sate Ayam';
+    restoData['openHours'] =
+        'Senin - Jumat: 08.00 - 21.00\nSabtu - Minggu: 09.00 - 22.00';
+  }
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: "Profil Restoran",
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        appBar: AppBar(title: const Text("Profil Restoran")),
       ),
     );
   }
